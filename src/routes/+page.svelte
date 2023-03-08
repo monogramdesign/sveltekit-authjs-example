7e06b45826535eb03d857869f3cde5c87d46bf4c
Austin Crim <austin@monogram.io>
2023-03-08 14:43:13 -0600

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+</script>
+
+<h1>Welcome {$page.data.session?.user?.name}</h1>
+<img
+  width="200"
+  src={$page.data.session?.user?.image}
+  alt="github profile avatar"
+/>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+  import { signIn, signOut } from '@auth/sveltekit/client'
+</script>
+
+{#if !$page.data.session}
+  <button on:click={() => signIn('github', { callbackUrl: '/' })}>
+    Sign in with GitHub
+  </button>
+{:else}
+  <button on:click={signOut}> Log out</button>
+{/if}
